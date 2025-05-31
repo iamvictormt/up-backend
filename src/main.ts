c517771693474bc81ee25174f9fb3254a87e5006
Victor <victoorres@icloud.com>
2025-05-31 18:09:26 -0300

    fix: revert commit

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,38 @@
+import { Handler, Context, Callback } from 'aws-lambda';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import serverlessExpress from '@vendia/serverless-express';
+
+let server: Handler;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const expressApp = express();
+
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
 
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: [
-      'https://up-landing-page.vercel.app',
-      'http://localhost:3000',
-      'http://localhost:3001',
+      'https://up-landing-page.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3002);
+  await app.init();
+
+  server = serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler = async (event: any, context: Context, callback: Callback) => {
+  if (!server) {
+    await bootstrap();
+  }
+  return server(event, context, callback);
+};
