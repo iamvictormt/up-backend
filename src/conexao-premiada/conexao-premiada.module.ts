1e809342b161a641991867ed45ad99ea283f541f
macbook <macbook@192.168.1.5>
2026-04-11 05:18:38 -0300

    fix guard in conexao premiada

--- src/conexao-premiada/conexao-premiada.module.ts
+++ src/conexao-premiada/conexao-premiada.module.ts
@@ -3,9 +3,10 @@ import { ConexaoPremiadaService } from './conexao-premiada.service';
 import { ConexaoPremiadaController } from './conexao-premiada.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PointsModule } from 'src/points/points.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, PointsModule],
+  imports: [PrismaModule, PointsModule, AuthModule],
   controllers: [ConexaoPremiadaController],
   providers: [ConexaoPremiadaService],
   exports: [ConexaoPremiadaService],
